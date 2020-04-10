49dd7422f5b9af5cd69a1da67922d2f0c04e160f
sma20 <56293757+sma20@users.noreply.github.com>
2020-04-11 00:52:33 +0100

    Update Speech_Handlers.py

--- bot/Speech_Handlers.py
+++ bot/Speech_Handlers.py
@@ -1,17 +1,56 @@
 import os
+import socket
+import time
 
 class Speech_Handlers:
 
     def __init__(self):
+
         pass
 
     def asr(self):
-        user_input = input(">> Your utterance:\n")
+        #data = 'asr - get speech data'
+        #client = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+        #client.connect(('localhost', 9999))
+        #client.send(data.encode())
+        #user_input = client.recv(4096)
+        #user_input = (user_input.decode('utf-8'))
+        #client.close()
 
-        if user_input.strip() == "":
-            return False
 
+        f = open("furhat.txt", "r")
+
+        user_input = str(f.read())
+
+        #user_input = input(">> Your utterance:\n")
+
+        #if user_input.strip() == "":
+        #    return False
+
+        while not user_input:
+            f = open("furhat.txt", "r")
+
+            user_input = str(f.read())
+
+        print (user_input)
+        f = open("furhat.txt", "w")
+        f.write("")
+        f.close()
         return user_input
 
+
     def tts(self, utterance):
-        print(utterance)
+        #utterance = "from the bot+"+utterance
+        #client = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+        #client.connect(('localhost', 9999))
+        #client.send(utterance.encode())
+
+        #client.close()
+        f = open("furhat.txt", "w")
+        f.write(utterance)
+        f.close()
+
+        f = open("resp.txt", "w")
+        f.write(utterance)
+        f.close()
+        time.sleep(4)
